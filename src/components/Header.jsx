76fb9def0416d19c0eb41c16f5f5d208b46a22ff
Dulaj Ayeshmantha <155412252+DulajAR@users.noreply.github.com>
2025-03-03 23:02:58 +0530

    Update header with improved navigation and mobile responsiveness
    Enhanced the header component by improving navigation, adding better styling, and ensuring mobile responsiveness. Adjusted layout and structure for a smoother user experience.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,20 @@
-// src/components/Header.js
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import logo from '../assets/logo.png'; 
 
 const Header = () => {
   return (
     <section id="header">
-      <Link to="/"><img src="logo.png" className="logo" alt="Sukkanama Logo" /></Link>
-      <div>
+      <img src={logo} className="logo" alt="Sukkanama Logo" />
+      <nav>
         <ul id="navbar">
           <li><Link to="/" className="active">Home</Link></li>
           <li><Link to="/loginSupplier">Rent Your Vehicle</Link></li>
           <li><Link to="/loginCustomer">Find Your Vehicle</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/contact">Contact</Link></li>
-          <a href="#" id="close"><i className="fa fa-window-close" aria-hidden="true"></i></a>
         </ul>
-      </div>
+      </nav>
       <div id="mobile">
         <Link to="/cart"><i className="fas fa-shopping-bag"></i></Link>
         <i id="bar" className="fas fa-outdent"></i>
