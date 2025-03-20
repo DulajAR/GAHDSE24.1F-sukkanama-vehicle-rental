f3dac506cf3d48e9d86c3a4bded5cfc66bca9929
Navodya <155411569+NDewa12@users.noreply.github.com>
2025-03-20 22:10:01 +0530

    add About page
    *Created a new About page with basic layout and content,
    *Applied initial styling for responsive design,
    *Linked About page to the main navigation

---
+++ src/pages/About.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "../components/Header";
+import AboutContent from "../components/AboutContent";
+import Features from "../components/Feature";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
+
+const About = () => {
+  return (
+    <div>
+      <Header />
+      <AboutContent />
+      <Features />
+      <Newsletter />
+      <Footer />
+    </div>
+  );
+};
+
+export default About;
