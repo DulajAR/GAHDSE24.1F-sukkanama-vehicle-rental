4a2cfcd691d7ae367152f79e85c6eba9ce70eba2
Dulaj Ayeshmantha <155412252+DulajAR@users.noreply.github.com>
2025-03-23 22:23:23 +0530

    Update the ContactFormSection.jsx component
    update the company  staff details

--- src/components/ContactFormSection.jsx
+++ src/components/ContactFormSection.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import img1 from "../assets/1.png"; // Adjust the path based on your folder structure
 import img2 from "../assets/2.png";
 import img3 from "../assets/3.png";
+import img4 from "../assets/4.png";
 
 const ContactFormSection = () => {
   return (
@@ -18,31 +19,43 @@ const ContactFormSection = () => {
 
       <div className="people">
         <div>
-          <img src={img1} alt="Kamal Perera" />
+          <img src={img1} alt="Dulaj Ayeshmantha" />
           <p>
-            <span>Kamal Perera</span> Senior Marketing Manager <br />
-            Phone: +94 776545432 <br />
-            Email: contact@gmail.com
+            <span>Dulaj Ayeshmantha</span> CEO / Founder <br />
+            Phone: +94 788568282 <br />
+            Email: dulajayeshmantha91@gmail.com
           </p>
         </div>
 
         <div>
-          <img src={img2} alt="Standra Alwis" />
+          <img src={img2} alt="Felicia Gunasekara" />
           <p>
-            <span>Standra Alwis</span> Managing Director <br />
-            Phone: +94 716599832 <br />
-            Email: stan@gmail.com
+            <span>Felicia Gunasekara</span> Operations Manager <br />
+            Phone: +94 761825864 <br />
+            Email: sandeegunasekera@gmail.com
           </p>
         </div>
 
         <div>
-          <img src={img3} alt="Stafanie Fernando" />
+          <img src={img3} alt="Navodya Dewmini" />
           <p>
-            <span>Stafanie Fernando</span> Senior Marketing Supervisor <br />
-            Phone: +94 776544321 <br />
-            Email: hallo@gmail.com
+            <span>Navodya Dewmini</span> Customer Support Manager <br />
+            Phone: +94 753236372 <br />
+            Email: dnavodya049@gmail.com
           </p>
         </div>
+
+        <div>
+          <img src={img4} alt="Saveena Sathsaranee" />
+          <p>
+            <span>Saveena Sathsaranee</span> Technical Lead / Developer <br />
+            Phone: +94 764806258 <br />
+            Email: saveenaanabel@gmail.com
+          </p>
+        </div>
+
+
+
       </div>
     </section>
   );
